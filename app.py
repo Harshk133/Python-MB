3c3b23a289ecbce045e5efd0e3bb379a217caa71
Harsh Moreshwar Kale <h@rshmkale.2004@gmail.com>
2024-04-09 12:03:22 +0530

    #5

--- app.py
+++ app.py
@@ -85,14 +85,6 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('login'))
 
-# @app.route("/")
-# def index():
-#     if 'username' in session:
-#         username = session['username']
-#         return render_template('index.html', username=username)
-#     else:
-#         return redirect(url_for('login'))  # Redirect to login page if not logged in
-
 # ! Do not remove this 💀
 @app.route("/")
 def index():
@@ -104,7 +96,6 @@ def index():
         return redirect(url_for('login'))
     
 
-
 #~ Function to check if the file extension is allowed
 def allowed_file(filename):
     return '.' in filename and \
@@ -114,14 +105,11 @@ def allowed_file(filename):
 @app.route('/profile')
 def profile():
     try:
-        # Get the user ID from the session
         username = session.get('username')
         print("the value of the user id is", username)
 
-        # Retrieve the list of document IDs starred by the user
         starred_documents = db.stars.find({'user_id': username}, {'document_id': 1})
 
-        # Retrieve the details of the starred documents
         documents = []
         for starred_doc in starred_documents:
             document_id = starred_doc['document_id']
@@ -129,34 +117,27 @@ def profile():
             if document:
                 documents.append(document)
 
-        # query to getting the star documents
         starred_count = db.stars.count_documents({'user_id': username})
 
-        # Retrieve the documents used by the user
         used_documents = db.template_cards.find({'used_by': username})
 
-        # Retrieve documents created by the user
         user_documents = db.template_cards.find({'creator': username})
 
         return render_template('profilepage.html', username=session.get("username"), starred_documents=documents, starred_count=starred_count, used_documents=used_documents, user_documents=user_documents)
 
     except Exception as e:
         print(f"Error: {str(e)}")
-        # Handle error
         return jsonify({'success': False, 'error': str(e)})
 
 
 @app.route("/otherprofile/<username>")
 def otherprofile(username):
     try:
-        # Retrieve user profile data from the database
         person = db.template_cards.find({'creator': username})
 
         if person:
-            # Retrieve documents created by the user
             user_documents = db.template_cards.find({'creator': username})
 
-            # Retrieve documents used by the user
             used_documents = db.template_cards.find({'used_by': username})
 
             return render_template('otherprofilepage.html', username=username, used_documents=used_documents, user_documents=user_documents)
@@ -186,10 +167,8 @@ def submit_upload():
         document_file = request.files['document']
         files = request.files.getlist('images[]')
 
-        # Get the username from the session
         creator = session.get("username")
 
-        # Check if the post request has the file part
         if 'images[]' not in request.files:
             flash('No file part')
             return redirect(request.url)
@@ -219,7 +198,6 @@ def submit_upload():
                 flash('Invalid file format')
                 return redirect(request.url)
         
-        # Insert document template card into the database
         db.template_cards.insert_one({
             "title": title,
             "description": description,
@@ -239,16 +217,9 @@ def submit_upload():
 
     return "Invalid request"
 
-from flask import render_template
-
 @app.route('/use_this/<document>')
 def use_this(document):
-    # Assuming you have the document name stored in the database
-    # You can retrieve the document path or details from the database based on the document name
-    # For now, I'll assume you have the path to the document file
     document_path = get_document_path_from_database(document)
-
-    # Redirect the user to the page where they can enter data for the placeholders
     return redirect(url_for('enter_data', document=document, document_path=document_path))
 
 
@@ -271,10 +242,8 @@ def enter_data():
     return render_template('enter_data.html', document=document, placeholders=placeholders)
 
 def get_document_path_from_database(document_name):
-    # db = db
     print("teh name of the document is see here", document_name)
     document = db.template_cards.find_one({"title": document_name})
-    # document = db.template_cards.find_one({"title": "Template"})
 
     if document:
         document_path = document.get("document")
@@ -290,58 +259,44 @@ def submit_data():
         document_name = request.form["documentName"]
         document_path = get_document_path_from_database(document_name)
 
-        # Extract placeholders from the document
         placeholders = extract_placeholders(document_path)
         print("Placeholders from document:", placeholders)
 
-        # Get the form data as a regular Python dictionary
         form_data_dict = {key.strip(): value for key, value in request.form.items()}
         print("Form Data:", form_data_dict)
 
-        # Fill the document with user input
         filled_document_path = fill_document(document_path, form_data_dict)
 
-        # Send the filled document for download
         return send_file(filled_document_path, as_attachment=True)
 
     return 'Form submitted successfully'
 
 def fill_document(document_path, placeholders_values):
-    # Use DocxTemplate or any other method to fill placeholders in the document
     doc = DocxTemplate(document_path)
     doc.render(placeholders_values)
     
-    # Save the filled document temporarily
     filled_document_path = tempfile.NamedTemporaryFile(suffix='.docx', delete=False).name
     doc.save(filled_document_path)
     
     return filled_document_path
 
 # ================== some essential actions ==================
-# Route to handle incrementing stars
 from bson import ObjectId
 
 # ^ route for increamenting the star count
 @app.route('/star/<document_id>', methods=['POST'])
 def star_document(document_id):
     try:
-        # Get the user ID or username from the session or request
         user_id = session.get('username')  # If user ID is stored in the session
         print("user id is", user_id, session)
-        # Alternatively, you can use some other unique identifier from the request,
-        # such as username or user ID retrieved from the request data.
 
-        # Check if the document exists
         document = db.template_cards.find_one({'_id': ObjectId(document_id)})
 
         if document:
-            # Check if the user has already starred the document
             existing_star = db.stars.find_one({'document_id': document_id, 'user_id': user_id})
 
             if existing_star:
-                # If the user has already starred the document, remove the star
                 db.stars.delete_one({'document_id': document_id, 'user_id': user_id})
-                # Decrement the stars count for the document
                 db.template_cards.update_one(
                     {'_id': document['_id']},
                     {'$inc': {'stars': -1}}
@@ -349,9 +304,7 @@ def star_document(document_id):
                 star_count = document['stars'] - 1
                 return jsonify({'success': True, 'star_count': star_count, 'action': 'unstar'})
             else:
-                # If the user has not starred the document, add the star
                 db.stars.insert_one({'document_id': document_id, 'user_id': user_id})
-                # Increment the stars count for the document
                 db.template_cards.update_one(
                     {'_id': document['_id']},
                     {'$inc': {'stars': 1}}
@@ -377,23 +330,20 @@ def use_document(document_id):
         username = session.get('username')  # If user ID is stored in the session
         print("user id is", session, username)
 
-        # Update the document's "used by" count
         db.template_cards.update_one(
             {'_id': ObjectId(document_id)},
             {'$inc': {'uses': 1}}
         )
 
-        # Add the user to the document's "used by" list
         db.template_cards.update_one(
             {'_id': ObjectId(document_id)},
             {'$addToSet': {'used_by': username}}
         )
 
-        # Update or insert the document usage count in a separate collection
         db.document_usage.update_one(
             {'document_id': document_id},
             {'$inc': {'count': 1}},
-            upsert=True  # If the document is not found, create a new entry
+            upsert=True  
         )
 
         return jsonify({'success': True})
@@ -407,15 +357,11 @@ from flask import request, render_template
 
 @app.route('/search', methods=['GET'])
 def search():
-    # Get the search query from the request
     search_query = request.args.get('q')
 
     if search_query:
-        # Perform search query in the database (replace this with your actual database query)
-        # Assuming you have a collection named 'template_cards' in your B
         search_results = db.template_cards.find({'title': {'$regex': search_query, '$options': 'i'}})
 
-        # Pass search results to the template for rendering
         return render_template('index.html', search_results=search_results, search_query=search_query)
     else:
         return '<h1>Not Found!</h1>'
@@ -430,7 +376,6 @@ def delete_document():
         if not document_id:
             return jsonify({"error": "Document ID not provided"}), 400
 
-        # Delete the document template card from the database
         result = db.template_cards.delete_one({"_id": ObjectId(document_id)})
 
         if result.deleted_count == 1:
@@ -444,17 +389,14 @@ from flask import flash, redirect, render_template, request, url_for
 @app.route("/update_document/<string:document_id>", methods=["GET", "POST"])
 def update_document(document_id):
     if request.method == "GET":
-        # Retrieve the existing document from the database
         document = db.template_cards.find_one({"_id": ObjectId(document_id)})
         if not document:
             flash("Document not found")
             return redirect(url_for("index"))
 
-        # Render the update document template with the existing data
         return render_template("update_document.html", document=document)
     
     elif request.method == "POST":
-        # Extract form data
         title = request.form.get("document_title")
         document_type = request.form.get("dt")
         amount_price = request.form.get("amount")
